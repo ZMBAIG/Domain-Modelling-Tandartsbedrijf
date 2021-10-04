fecd17ba8da206d88a518c376b53993b7bd91431
Zafar Baig <49963556+ZMBAIG@users.noreply.github.com>
2021-10-05 00:16:36 +0200

    Add files via upload

---
+++ src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <h1 className="title">Tandartspraktijk B.V.T.!</h1>
+        </div>
+      </div>
+    );
+  }
+}
